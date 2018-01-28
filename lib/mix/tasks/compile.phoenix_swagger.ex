03e224e0c6580ee352b5cd6ca4a54e9e9e3181df
Michael Buhot <m.buhot@gmail.com>
2018-01-28 15:03:08 +1000

    Add compile.phoenix_swagger compiler task
    Compilers are required to follow the naming convention

---
+++ lib/mix/tasks/compile.phoenix_swagger.ex
@@ -0,0 +1,14 @@
+defmodule Mix.Tasks.Compile.PhoenixSwagger do
+  use Mix.Task.Compiler
+
+  @shortdoc "Compiles swagger annotations to JSON file"
+
+  @moduledoc """
+  See documentation for `Mix.Tasks.Phx.Swagger.Generate`
+  """
+
+  def run(_args) do
+    # TODO: this should intelligently track if the JSON files need to be updated
+    Mix.Task.run("phx.swagger.generate")
+  end
+end
